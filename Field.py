d5ffcdb16a83023adc1485f6e06e16d9c6af60a1
jamieHH <jamiehh@live.com>
2014-06-30 11:01:42 +0100

    Field task 13 complete
    Dictionary statements are not in the same order as shown in tutoreals.

--- Field.py
+++ Field.py
@@ -60,8 +60,44 @@ class Field:
                 water = needs['water need']
         return {'food': food,'light': light,'water': water}
 
-    def grow(self,):
-        pass
+    def grow(self,light,food,water):
+        #grow the crops (light and water are avalable to all crops in same amonts)
+        if len(self._crops) > 0:
+            for crop in self._crops:
+                crop.grow(light,water)
+        if len(self._animals) > 0:
+            #grow the animals (water avalable to all animals in ther same amounts)
+            #but food is a total that must be shared
+            food_required = 0
+            #get a total of the food required by the amimals in the field
+            for animal in self._animals:
+                needs = animal.needs()
+                food_required += needs['food need']
+            #if we have more food avalable than required work out the aditional avalable food
+            if food > food_required:
+                additional_food = food - food_required
+                food = food_required
+            else:
+                additional_food = 0
+            #grow each animal
+                for animal in self._animals:
+                    #get the animals food needs
+                    needs = animal.needs()
+                    if food >= needs['food need']:
+                        #remove food for this animal from total
+                        food -= needs['food need']
+                        feed = needs['food need']
+                        #see if any additional food to give
+                        if additional_food > 0:
+                            #remove some aditional food for this animal
+                            additional_food -= 1
+                            #add this to the feed to be given to the animal
+                            feed += 1
+                        #grow the animal
+                        animal.grow(feed,water)
+                        
+                    
+
 
 def display_crops(crop_list):
     print()
@@ -115,6 +151,9 @@ def main():
     print(report)
     report = new_field.report_needs()
     print(report)
+
+    new_field.grow(10,10,6)
+    print(new_field.report_contents())
 
 
 if __name__ == "__main__":
